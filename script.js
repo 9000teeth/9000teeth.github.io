0f397acdb1b6dbb321ee190ea4a7ec55ddd47064
9000teeth <86176798+9000teeth@users.noreply.github.com>
2021-06-20 00:00:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function($){
 $(document).ready(function(){
 	var day = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"][(new Date()).getDay()];
-	$(".day").html(day);
+	$(".day").html("sunday");
 });
 })(jQuery);
